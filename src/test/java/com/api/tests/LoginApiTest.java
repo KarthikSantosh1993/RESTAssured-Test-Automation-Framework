3d359aa6fb049379f64c445ec5636c70ec5c0403
Karthik <krthiksantosh@gmail.com>
2025-11-21 23:43:16 +0530

    initial Project structure

---
+++ src/test/java/com/api/tests/LoginApiTest.java
@@ -0,0 +1,33 @@
+package com.api.tests;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
+import com.api.pojo.UserCredentials;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class LoginApiTest {
+
+	@Test
+	public void loginAPITest() {
+
+		UserCredentials userCredentials= new UserCredentials("iamfd", "password");
+		
+		given()
+			.baseUri("http://64.227.160.186:9000/v1/").and()
+			.contentType(ContentType.JSON)
+			.body(userCredentials)
+			.log().all()
+		.when()
+			.post("login")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.and()
+			.body("message", equalTo("Success"))
+			.and()
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/LoginResponseSchema.json"));
+
+	}
+}
