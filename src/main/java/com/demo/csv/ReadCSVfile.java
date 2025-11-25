901d26ede56841286ef3e03721ffd600b9fac4cf
Karthik <krthiksantosh@gmail.com>
2025-11-25 23:30:42 +0530

    Requirement:8 Read  test data from CSV (initial development)
    worked with reading CSV file

---
+++ src/main/java/com/demo/csv/ReadCSVfile.java
@@ -0,0 +1,29 @@
+package com.demo.csv;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+public class ReadCSVfile {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException, CsvException {
+		InputStream inputStream = Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream("testdata/logincreds.csv");
+		System.out.println(Thread.currentThread().getContextClassLoader().getResourceAsStream("").toString());
+		InputStreamReader streamReader = new InputStreamReader(inputStream);
+		CSVReader csvReader = new CSVReader(streamReader);
+		List<String[]> dataList = csvReader.readAll();
+
+		for (String[] dataArray : dataList) {
+			System.out.println(dataArray[0]);
+			System.out.println(dataArray[1]);
+		}
+
+	}
+
+}
