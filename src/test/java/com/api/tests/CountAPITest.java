aeb59debbefe584e69b9e2dd39bbc2195dd63862
Karthik <krthiksantosh@gmail.com>
2025-11-25 17:28:07 +0530

    Added description and groups to test methods
    Introduced the setup method for removing the pay load creation outside
    test

--- src/test/java/com/api/tests/CountAPITest.java
+++ src/test/java/com/api/tests/CountAPITest.java
@@ -1,7 +1,7 @@
 package com.api.tests;
 
 import static com.api.constants.Role.FD;
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
@@ -10,30 +10,30 @@ import java.io.IOException;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
-import com.api.utils.SpecUtil;
+import static com.api.utils.SpecUtil.*;
 
-import io.restassured.module.jsv.JsonSchemaValidator;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class CountAPITest {
-	@Test
+	
+	@Test(description="Verify count API response is shown correctly", groups = {"api", "smoke", "regression"})
 	public void verifyCountAPIRequest() throws IOException {
-		given().spec(SpecUtil.requestSpecificationWithAuth(FD))
+		given().spec(requestSpecificationWithAuth(FD))
 		.when().get("dashboard/count")
-		.then().spec(SpecUtil.responseSpec_OK())
+		.then().spec(responseSpec_OK())
 		.body("data", notNullValue()).body("data.size()", equalTo(3))
 				.body("data.count", Matchers.everyItem(Matchers.greaterThanOrEqualTo(0)))
 				.body("data.label", Matchers.everyItem(Matchers.not(Matchers.blankOrNullString())))
 				.body("data.key",
 						Matchers.containsInAnyOrder("pending_fst_assignment", "created_today", "pending_for_delivery"))
-				.body(JsonSchemaValidator
-						.matchesJsonSchemaInClasspath("response-schema/CountAPIResponseSchema_FD.json"));
+				.body(matchesJsonSchemaInClasspath("response-schema/CountAPIResponseSchema_FD.json"));
 	}
 
-	@Test
+	@Test(description="Verify count API is giving correct status code for invalid token", groups = {"api", "negative", "smoke", "regression"})
 	public void countAPI_AuthToken_negative() throws IOException {
-		given().spec(SpecUtil.requestSpec())
+		given().spec(requestSpec())
 		.when().get("dashboard/count")
-		.then().spec(SpecUtil.responseSpec_HTML(401));
+		.then().spec(responseSpec_HTML(401));
 	}
 	
 }
--- src/test/java/com/api/tests/MasterAPIRequestTest.java
+++ src/test/java/com/api/tests/MasterAPIRequestTest.java
@@ -1,33 +1,33 @@
 package com.api.tests;
 
-import static com.api.constants.Role.*;
-import static io.restassured.RestAssured.*;
+import static com.api.constants.Role.FD;
+import static io.restassured.RestAssured.given;
 
 import java.io.IOException;
 
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
-import com.api.utils.SpecUtil;
+import static com.api.utils.SpecUtil.*;
 
-import io.restassured.module.jsv.JsonSchemaValidator;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class MasterAPIRequestTest {
 
-	@Test
+	@Test(description="Verify master API response is shown correctly", groups = {"api", "smoke", "regression"})
 	public void masterAPIRequest() throws IOException {
-		given().spec(SpecUtil.requestSpecificationWithAuth(FD)) 	// application/url-formencoded	
-		.when().post("master").then().spec(SpecUtil.responseSpec_OK())
+		given().spec(requestSpecificationWithAuth(FD)) 	// application/url-formencoded	
+		.when().post("master").then().spec(responseSpec_OK())
 				.body("message", Matchers.equalTo("Success")).body("data", Matchers.notNullValue())
 				.body("data", Matchers.hasKey("mst_oem")).body("$", Matchers.hasKey("message"))
 				.body("data.mst_oem.size()", Matchers.equalTo(2)) // check size of JSON Array
 				.body("data.mst_oem.id", Matchers.everyItem(Matchers.notNullValue()))
 				.body("data.mst_oem.name", Matchers.everyItem(Matchers.notNullValue()))
-				.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/MasterAPIResponseSchema.json"));
+				.body(matchesJsonSchemaInClasspath("response-schema/MasterAPIResponseSchema.json"));
 	}
 	
-	@Test
+	@Test(description="Verify master API is giving correct status code for invalid token", groups = {"api", "negative", "smoke", "regression"})
 	public void invalidTokenMasterAPirequest() throws IOException {
-		given().spec(SpecUtil.requestSpec()).when().post("master").then().spec(SpecUtil.responseSpec_HTML(401));
+		given().spec(requestSpec()).when().post("master").then().spec(responseSpec_HTML(401));
 	}
 }
