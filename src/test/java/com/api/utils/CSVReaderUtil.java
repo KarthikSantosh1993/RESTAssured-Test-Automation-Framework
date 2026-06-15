25e7051026296e7f5ce52b0e5da6ff8ac9763bd3
Karthik Duruvasula <krthiksantosh@gmail.com>
2026-06-15 20:30:35 +0530

    Introduced login api data driven tests for open csv library

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -2,6 +2,7 @@ package com.api.utils;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.List;
 
 import com.dataproviders.api.bean.UserBean;
@@ -10,11 +11,10 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CSVReaderUtil {
-
 	private CSVReaderUtil() {
 	}
 
-	public static void loadCSV(String pathOfCSVFile) {
+	public static Iterator<UserBean> loadCSV(String pathOfCSVFile) {
 		InputStream inputStream = Thread.currentThread().getContextClassLoader()
 				.getResourceAsStream(pathOfCSVFile);
 		InputStreamReader reader = new InputStreamReader(inputStream);
@@ -25,7 +25,6 @@ public class CSVReaderUtil {
 				.withIgnoreEmptyLine(true)
 				.build();
 		List<UserBean> userList = csvToBean.parse();
-		System.out.print(userList.get(0));
+		return userList.iterator();
 	}
-
 }
